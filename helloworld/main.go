97a5c42636306b4ce5c3b408dd03afd7b25d28a7
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2025-10-29 22:23:43 -0300

    init go learning

---
+++ helloworld/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world from go lang, by Jean!")
+	fmt.Println("Hello Caroline!")
+}
---
+++ mars/mars.go
@@ -0,0 +1,13 @@
+// My weight loss program
+package main
+
+import "fmt"
+
+// main is the function where it all begins
+func main() {
+	fmt.Print("My weight on the surface of Mars is ")
+	fmt.Print(60 * 0.3783)
+	fmt.Print(" kg, and I would be ")
+	fmt.Print(27 * 365 / 687)
+	fmt.Print(" years old.")
+}
