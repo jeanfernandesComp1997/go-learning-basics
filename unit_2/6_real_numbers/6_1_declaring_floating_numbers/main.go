397bb23b5bce9be5fe0b6920d276ad2b11d5a11f
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2025-11-09 14:10:18 -0300

    add real numbers

---
+++ unit_2/6_real_numbers/6_1_declaring_floating_numbers/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	days := 365.2425
+	// var days = 365.2425
+	// var days float64 = 365.2425
+
+	var answer float64 = 42
+
+	fmt.Println(days)
+	fmt.Println(answer)
+}
