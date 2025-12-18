950cb2c773fcddc3e78ec70515892331ba6bbf9d
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2025-12-17 23:05:24 -0300

    add slicing default

---
+++ unit_4/17_slices_windows_into_arrays/17_1_1_default_indices_for_slicing/slicing-default.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	planets := [...]string{
+		"Mercury",
+		"Venus",
+		"Earth",
+		"Mars",
+		"Jupiter",
+		"Saturn",
+		"Uranus",
+		"Neptune",
+	}
+
+	terrestrial := planets[:4]
+	gasGiants := planets[4:6]
+	iceGiants := planets[6:]
+
+	fmt.Println(terrestrial, gasGiants, iceGiants)
+
+	allPlanets := planets[:]
+	fmt.Println(allPlanets)
+
+	neptune := "Neptune"
+	tune := neptune[3:]
+	fmt.Println(tune)
+
+	neptune = "Poseidon"
+	fmt.Println(tune)
+
+	question := "¿Cómo estás?"
+	fmt.Println(question[:6])
+
+	colonizedPlanets := planets[2:4]
+	fmt.Println(colonizedPlanets)
+}
