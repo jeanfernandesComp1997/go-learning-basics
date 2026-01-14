5ba4c693aca293e00327efd218df87dab8bb01c2
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2026-01-13 22:53:30 -0300

    add maps

---
+++ unit_4/19_the_ever_versatile_map/19_3_preallocating_maps_with_make/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var temperature = map[float64]int{}
+
+	temperatue2 := make(map[float64]int)
+
+	fmt.Println(len(temperature))
+	fmt.Println(len(temperatue2))
+}
