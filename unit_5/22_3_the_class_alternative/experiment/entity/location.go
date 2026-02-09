9635723b34587ce4f4afe4ae526a47380e687b15
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2026-02-09 19:48:20 -0300

    add classes alternatives

--- unit_5/22_3_the_class_alternative/experiment/entity/location.go
+++ unit_5/22_3_the_class_alternative/experiment/entity/location.go
@@ -8,8 +8,19 @@ import (
 )
 
 type Location struct {
-	Lat  float64 `json:"latitude"`
-	Long float64 `json:"longitude"`
+	Name        string  `json:"name"`
+	LandingSite string  `json:"landingSite"`
+	Lat         float64 `json:"latitude"`
+	Long        float64 `json:"longitude"`
+}
+
+func NewLocation(name, landingSite string, latCoord, longCoord Coordinate) Location {
+	return Location{
+		Name:        name,
+		LandingSite: landingSite,
+		Lat:         roundToDecimals(latCoord.Decimal(), 4),
+		Long:        roundToDecimals(longCoord.Decimal(), 4),
+	}
 }
 
 func (location Location) Print() {
@@ -26,13 +37,22 @@ func exitOnError(err error) {
 }
 
 func roundToDecimals(value float64, decimals int) float64 {
-	multipler := math.Pow(10, float64(decimals))
-	return math.Round(value*multipler) / multipler
+	multiplier := math.Pow(10, float64(decimals))
+	return math.Round(value*multiplier) / multiplier
 }
 
-func (location Location) MarshalJSON() ([]byte, error) {
-	return json.Marshal(map[string]float64{
-		"latitude":  roundToDecimals(location.Lat, 4),
-		"longitude": roundToDecimals(location.Long, 4),
-	})
-}
+// func (location Location) MarshalJSON() ([]byte, error) {
+// 	type Alias Location
+
+// 	return json.Marshal(&struct {
+// 		Name        string  `json:"name"`
+// 		LandingSite string  `json:"landingSite"`
+// 		Lat         float64 `json:"latitude"`
+// 		Long        float64 `json:"longitude"`
+// 	}{
+// 		Name:        location.Name,
+// 		LandingSite: location.LandingSite,
+// 		Lat:         roundToDecimals(location.Lat, 4),
+// 		Long:        roundToDecimals(location.Long, 4),
+// 	})
+// }
--- unit_5/22_3_the_class_alternative/experiment/landing.go
+++ unit_5/22_3_the_class_alternative/experiment/landing.go
@@ -5,9 +5,39 @@ import (
 )
 
 func main() {
-	spiritLatCoordinate := entity.Coordinate{D: 14, M: 34, S: 6.2, H: 'S'}
-	spiritLongCoordinate := entity.Coordinate{D: 175, M: 28, S: 24.2, H: 'E'}
-
-	spirit := entity.Location{Lat: spiritLatCoordinate.Decimal(), Long: spiritLongCoordinate.Decimal()}
+	// Spirit
+	spirit := entity.NewLocation(
+		"Spirit",
+		"Columbia Memorial Station",
+		entity.Coordinate{D: 14, M: 34, S: 6.2, H: 'S'},
+		entity.Coordinate{D: 175, M: 28, S: 21.5, H: 'E'},
+	)
 	spirit.Print()
+
+	// Opportunity
+	opportunity := entity.NewLocation(
+		"Opportunity",
+		"Challenger Memorial Station",
+		entity.Coordinate{D: 1, M: 56, S: 46.3, H: 'S'},
+		entity.Coordinate{D: 354, M: 28, S: 24.2, H: 'E'},
+	)
+	opportunity.Print()
+
+	// Curiosity
+	curiosity := entity.NewLocation(
+		"Curiosity",
+		"Bradbury Landing",
+		entity.Coordinate{D: 4, M: 35, S: 22.2, H: 'S'},
+		entity.Coordinate{D: 137, M: 26, S: 30.1, H: 'E'},
+	)
+	curiosity.Print()
+
+	// InSight
+	insight := entity.NewLocation(
+		"InSight",
+		"Elysium Planitia",
+		entity.Coordinate{D: 4, M: 30, S: 0.0, H: 'N'},
+		entity.Coordinate{D: 135, M: 54, S: 0, H: 'E'},
+	)
+	insight.Print()
 }
